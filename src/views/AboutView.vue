9d99493c9914b8ee6ffde2ca6b6eb5f1a0dfb27f
Mudit Gurjar <mudit182@gmail.com>
2022-03-12 00:48:05 +0530

    - added tailwind.css to project

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">Hello 👋🏼</div>
   </div>
 </template>
