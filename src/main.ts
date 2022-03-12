1be0762349690a596ae253a1eb89d5cd5eed15dc
Mudit Gurjar <mudit182@gmail.com>
2022-03-12 10:31:35 +0530

    - removed vuex & store

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import store from "./store";
 import "./styles/app.css";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(router).mount("#app");
