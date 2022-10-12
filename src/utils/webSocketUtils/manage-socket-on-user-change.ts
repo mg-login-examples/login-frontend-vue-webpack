848dda73a4de360a520d414bdd48639bc15fb52d
Mudit Gurjar <mudit182@gmail.com>
2022-10-13 00:21:31 +0530

    - fixed pinia bug

--- src/utils/webSocketUtils/manage-socket-on-user-change.ts
+++ src/utils/webSocketUtils/manage-socket-on-user-change.ts
@@ -1,10 +1,10 @@
 import { useUserStore } from "@/store/user";
 import { useWebSocketStore } from "@/store/webSocket";
 
-const userStore = useUserStore();
-const webSocketStore = useWebSocketStore();
-
 export function connectDisconnectSocketOnUserLogin() {
+  const userStore = useUserStore();
+  const webSocketStore = useWebSocketStore();
+
   userStore.$subscribe((a, userState) => {
     if (userState.user) {
       // user login or authenticate
